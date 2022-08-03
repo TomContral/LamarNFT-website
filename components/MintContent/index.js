b028149f7ae4feb06e3de6d29729cd229cfbdc06
TomContral <43318472+TomContral@users.noreply.github.com>
2022-08-03 11:23:21 +0800

    nft-react  001
    实现了mint页面数字按钮的功能

--- components/MintContent/index.js
+++ components/MintContent/index.js
@@ -1,10 +1,27 @@
 import styles from './index.module.scss'
+import { React, useState } from 'react'
+
 export default function MintContent(){
+
+    const [counter, setCounter] = useState(1);
+
+    const decrementCounter = () => {
+        if (counter <= 1)
+            return;
+        setCounter(counter - 1);
+    };
+
+    const incrementCounter = () => {
+        if (counter >= 3)
+            return;
+        setCounter(counter + 1);
+    };
+    const maxCounter = () => setCounter(3);
+
     return (
         <div className={styles.container}>
             <img src='/images/productImg.png' style={{width:'624px'}}/>
             <div className={styles.rightContent}>
-                <div className={styles.title}>Make sure to connect your Wallet before purchasing</div>
                 <div className={styles.row}>
                     <div className={styles.left}>Total sold</div>
                     <div className={styles.right}>0 of 7,777</div>
@@ -13,20 +30,25 @@ export default function MintContent(){
                     <div className={styles.left}>Amount</div>
                     <div className={styles.right}>
                         <div className={styles.num}>
-                            <div className={styles.operateBtn}>-</div>
+                            <div className={styles.operateBtn}
+                                 onClick={decrementCounter}
+                            >-</div>
                             <div style={{
                                 color:'#68F9DC',
                                 fontSize:'32px',
                                 margin: '0 37px'
-                            }}>1</div>
-                            <div className={styles.operateBtn}>+</div>
+                            }}>{counter}</div>
+                            <div className={styles.operateBtn}
+                                 onClick={incrementCounter}
+                            >+</div>
                         </div>
-                        <div className={styles.gradientBtn}></div>
+                        <div className={styles.gradientBtn}
+                             onClick={maxCounter}></div>
                     </div>
                 </div>
                 <div className={styles.row}>
                     <div className={styles.left}>Total amount</div>
-                    <div className={styles.right}>0.0777 ETH</div>
+                    <div className={styles.right}>{0.02*counter}ETH</div>
                 </div>
                 <div className={styles.btn}>Coming soon</div>
             </div>
